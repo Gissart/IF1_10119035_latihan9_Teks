9bbe1f17c461614074c64909cd79d7fe0f1597ab
unknown <argya.programmer@gmail.com>
2020-10-17 14:30:31 +0700

    Initial Commit

---
+++ src/com/tugasPBO/latihanTeks.java
@@ -0,0 +1,16 @@
+//latihan 9 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan teks dengan tab \t dan baris baru \n
+package com.tugasPBO;
+
+public class latihanTeks {
+    public static void main(String[] args){
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("hasil "+ newline+ "char : "+tab+ "berisi "+c);
+    }
+}
